7869803840f4d59de6ce3568358f6b3347ff6b58
pin20 <pin200048@gmail.com>
2021-12-24 21:20:02 +0300

    Подготовка к дз1

--- src/ru/lookBag/Main.java
+++ src/ru/lookBag/Main.java
@@ -3,6 +3,22 @@ package ru.lookBag;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Первое задание
+
+        byte Byte = 5;
+        short Short = -28659;
+        int Int = 326554854;
+        long Long = 5816545181519954324L;
+
+        float Float = 568.123786f;
+        double Double = -41.288452261154;
+
+        char Char = 'C';
+        boolean thisTrueOrFalse = true;
+
+    // Второе задание
+
+
+
     }
 }
